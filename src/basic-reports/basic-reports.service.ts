1fa5bdb355764133ec2786480738e39afe499f1c
Juan Valero <juanvalero@cosmo.edu.co>
2024-08-07 12:21:32 -0500

    Refactor y Modularización

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,18 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrinterService } from 'src/printer/printer.service';
+import { getHelloWorldReport } from 'src/reports';
 
-//TODO: Deberá ser optimizado más adelante
-import PdfPrinter from 'pdfmake';
-import type { TDocumentDefinitions } from 'pdfmake/interfaces';
-
-const fonts = {
-    Roboto: {
-        normal: 'fonts/Roboto-Regular.ttf',
-        bold: 'fonts/Roboto-Medium.ttf',
-        italics: 'fonts/Roboto-Italic.ttf',
-        bolditalics: 'fonts/Roboto-MediumItalic.ttf'
-    }
-};
 
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
@@ -20,15 +10,23 @@ export class BasicReportsService extends PrismaClient implements OnModuleInit {
         await this.$connect();
         // console.log('Connected to the database');
     }
+    constructor(
+        private readonly printerService: PrinterService
+    ) {
+        super();
+    }
 
     hello() {
-        const printer = new PdfPrinter(fonts);
 
-        const docDefinition: TDocumentDefinitions = {
-            content: ['Hello world!']
-        };
+        const docDefinition = getHelloWorldReport(
+            {
+                title: 'Hello World',
+                description: 'This is a simple hello world report',
+                name: 'John Doe'
+            }
+        );
 
-        const doc = printer.createPdfKitDocument(docDefinition)
+        const doc = this.printerService.createPdf(docDefinition)
         return doc;
     }
 
