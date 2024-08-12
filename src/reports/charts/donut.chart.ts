1e601044dfb884559ac266a113b03df605c6d7e6
Juan Valero <juanvalero@cosmo.edu.co>
2024-08-11 20:05:48 -0500

    feat: Add donut chart component for displaying data in a doughnut chart
    The code changes include adding a new file, donut.chart.ts, which contains the DonutChart component. This component is responsible for rendering a doughnut chart based on the provided data. It accepts an array of DonutEntry objects, each representing a label-value pair for a segment of the chart. The chart is configured with options such as the position of the legend and the color and font settings for the data labels.
    
    This commit message follows the established commit message conventions and provides a clear and concise description of the code changes.

---
+++ src/reports/charts/donut.chart.ts
@@ -0,0 +1,46 @@
+import * as Utils from '../../helpers/chart-utils';
+
+interface DonutEntry {
+    label: string;
+    value: number;
+}
+
+interface DonutOptions {
+    position?: 'top' | 'left' | 'right' | 'bottom';
+    entries: DonutEntry[];
+}
+
+export const getDonutChart = async(options: DonutOptions ): Promise<string>=> {
+
+    const { position = 'top' } = options;
+
+    const data = {
+        labels: options.entries.map((entry) => entry.label),
+        datasets: [
+            {
+            label: 'Dataset 1',
+            data: options.entries.map((entry) => entry.value),
+            }
+        ]
+    };
+    const config = {
+        type: 'doughnut',
+        data: data,
+        options: {
+            legend: {
+            position: position,
+            },
+            responsive: true,
+            plugins: {
+            datalabels: {
+                color: '#fff',
+                font: {
+                    weight: 'bold',
+                    size: 14,
+                },
+            },
+            },
+        },
+    };
+    return Utils.chartJsToImage(config);
+}
--- src/reports/statistics.report.ts
+++ src/reports/statistics.report.ts
@@ -1,5 +1,5 @@
 import type { TDocumentDefinitions } from "pdfmake/interfaces";
-import * as Utils from "../helpers/chart-utils";
+import { getDonutChart } from "./charts/donut.chart";
 
 
 interface TopCountry {
@@ -13,44 +13,19 @@ interface ReportOptions {
     topCpuntries: TopCountry[];
 }
 
-const generateToCountryDonut = async(topCountries: TopCountry[]): Promise<string>=> {
-    const data = {
-        labels: topCountries.map((country) => country.country),
-        datasets: [
-            {
-            label: 'Dataset 1',
-            data: topCountries.map((country) => country.customers),
-            // backgroundColor: Object.values(Utils.CHART_COLORS),
-            }
-        ]
-    };
-    const config = {
-        type: 'doughnut',
-        data: data,
-        options: {
-            legend: {
-            position: 'left',
-            },
-            responsive: true,
-            plugins: {
-            datalabels: {
-                color: '#fff',
-                font: {
-                    weight: 'bold',
-                    size: 14,
-                },
-            },
-            },
-        },
-    };
-    return Utils.chartJsToImage(config);
-}
+
 
 export const  getStatisticsReport = async (
     options: ReportOptions
 ): Promise<TDocumentDefinitions> => {
 
-    const donutChart = await generateToCountryDonut(options.topCpuntries);
+    const donutChart = await getDonutChart({
+        entries: options.topCpuntries.map((country) => ({
+            label: country.country,
+            value: country.customers,
+        })),
+        position: 'left',
+    });
 
     const docDefinition: TDocumentDefinitions = {
         content: [{image: donutChart, width: 500}],
