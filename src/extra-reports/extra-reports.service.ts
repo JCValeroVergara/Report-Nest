61558ad1462397bf0344b0366103e79cf6845c86
Juan Valero <juanvalero@cosmo.edu.co>
2024-08-12 16:44:01 -0500

    feat: Add html-to-pdfmake helper for converting HTML to PDFMake format
    The code changes include adding a new file, html-to-pdfmake.ts, which contains the getHtmlContent function. This function uses the html-to-pdfmake library and JSDOM to convert HTML content to PDFMake format. It is used to convert the HTML content of reports before generating PDFs. The html-to-pdfmake and jsdom dependencies are added to the package.json file.
    
    This commit message follows the established commit message conventions and provides a clear and concise description of the code changes.

--- src/extra-reports/extra-reports.service.ts
+++ src/extra-reports/extra-reports.service.ts
@@ -1,6 +1,10 @@
+import fs from 'fs';
 import { Injectable } from '@nestjs/common';
 import { PrinterService } from 'src/printer/printer.service';
 import { getHelloWorldReport } from 'src/reports';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
+import { getHtmlContent } from '../helpers/html-to-pdfmake';
+import { headerSection } from 'src/reports/sections/header.section';
 
 @Injectable()
 export class ExtraReportsService {
@@ -9,11 +13,17 @@ export class ExtraReportsService {
     ) {}
 
     getHtmlReport() {
-        const docDefinition = getHelloWorldReport({
-                title: 'Hello World',
-                description: 'This is a simple hello world report',
-                name: 'John Doe'
-            });
+
+        const html = fs.readFileSync('src/reports/html/basic-01.html', 'utf8');
+        const content = getHtmlContent(html);
+        const docDefinition: TDocumentDefinitions = {
+            pageMargins: [40, 110, 40, 60],
+            header: headerSection({
+                title: 'HTML to PDFMake',
+                subtitle: 'Convert HTML a PDFMake',
+            }),
+            content: content,
+        };
 
         const doc = this.printerService.createPdf(docDefinition)
 
